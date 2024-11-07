bc443643d85a92d29e79a813af83ca739192c37a
Matthew LettucePie <thelettucepie@gmail.com>
2024-11-07 16:13:14 -0500

    draft of island object database

---
+++ Scenes/PLAY/IslandObjectCompendium.gd
@@ -0,0 +1,66 @@
+extends Node
+class_name IOCompendium
+
+var compendium : Array = []
+
+
+class CompendiumEntry:
+	var io_name : String = ""
+	var io_scene_path : String = ""
+
+
+func _ready():
+	## TODO replace with some form of caching
+	rebuild_compendium_from_data()
+
+
+func rebuild_compendium_from_data():
+	_dir_contents("res://Scenes/Userspace/Decoration/")
+
+
+func _dir_contents(path):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+				_dir_contents(path + "/" + file_name)
+			else:
+				print("Found file: " + file_name)
+				if file_name.contains(".tscn"):
+					_catalog_io_scene(path + "/" + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+
+
+func _catalog_io_scene(path):
+	var scene_file : FileAccess = FileAccess.open(path, FileAccess.READ)
+	var content = scene_file.get_as_text()
+	var content_lines = content.split("\n", false)
+	for line in content_lines:
+		if line.contains("object_name = "):
+			var io_name : String = _rebuild_io_name(line - "object_name = ")
+			if !_check_repeat_by_io_name(io_name):
+				var new_entry : CompendiumEntry = CompendiumEntry.new()
+				new_entry.io_name = io_name
+				new_entry.io_scene_path = path
+	scene_file.close()
+
+
+func _check_repeat_by_io_name(io_name : String) -> bool:
+	var result : bool = false
+	
+	
+	
+	return result
+
+
+func _rebuild_io_name(io_name_raw) -> String:
+	var result : String = io_name_raw
+	
+	
+	
+	return result
