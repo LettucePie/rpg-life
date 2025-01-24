74771ca525d4ce0cf4298082e3cd0c20b54f54b8
Matthew LettucePie <thelettucepie@gmail.com>
2025-01-23 23:31:19 -0500

    instantiate doorway packer in plugin

--- addons/packer/packer.gd
+++ addons/packer/packer.gd
@@ -4,15 +4,20 @@ extends EditorPlugin
 ## TheBox
 
 var box_parser
+var doorway_packer
 
 func _enter_tree():
 	box_parser = preload("res://addons/packer/box_parser.gd").new()
 	box_parser.pack_me.connect(self._packing_time)
 	add_inspector_plugin(box_parser)
+	doorway_packer = preload("res://addons/packer/doorway_packer.gd").new()
+	doorway_packer.pack_me.connect(self._packing_time)
+	add_export_plugin(doorway_packer)
 
 
 func _exit_tree():
 	remove_inspector_plugin(box_parser)
+	remove_export_plugin(doorway_packer)
 
 
 func _packing_time(box : Box):
